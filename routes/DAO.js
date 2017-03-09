1b6027096c551081b850abf7db6be972942d1315
cdivyasudhakar <cdivyasudhakar@github.com>
2017-03-08 18:11:04 -0800

    first commit

---
+++ routes/DAO.js
@@ -0,0 +1,64 @@
+var mysql = require('mysql');
+var con = require('./connections');
+
+function connectDB(){
+	  var connection = mysql.createConnection(con.connectionString);
+	  
+connection.connect(function (err) {
+	        if (err) { throw err; }
+	    });
+	    return connection;
+}
+
+exports.fetch = function(selection, rows, fromDate, toDate, callback){
+	var select = "select";
+	  var from = "from";
+	  var tableName = "traffic_flow";
+	  var all = "*";
+	  var where = "where";
+	  var between = "between";
+	  var and = "and";
+	  var or = "or";
+	  var query = "";
+	  var timestamp = "timestamp";
+	  var between = "between"
+	  
+	  if(selection.length == 0)
+		  query = select+" "+all+" "+from+" "+tableName+" "+where;
+	  else
+		  query = select+" "+selection.join()+" "+from+" "+tableName+" "+where;
+	  
+	  query = query+" "+timestamp+" "+between+" "+fromDate+" "+and+" "+toDate;
+	  
+	  
+	if(rows.length !=0){
+		query = query+" "+and;
+		for(var row in rows){
+			var condition = rows[row];
+			if(condition.booleanOperator != null)
+				query = query+" "+condition.booleanOperator+" "+condition.column_name+" "+condition.operator;
+			else
+				query = query+" "+condition.column_name+" "+condition.operator;
+			
+			if(typeof condition.value === 'string')
+				query = query +" '"+condition.value+"'";
+			else
+				query = query +" "+condition.value;
+		}
+		
+	}
+	
+	var connection = connectDB();
+	
+	connection.query(query, function (err, rows, fields) {
+		if(err)
+			throw err;
+      if(rows.length >0)
+    	  	callback({status : 200, data : rows});
+    	  else
+    		callback({status : 400, data : "No rows"});  
+      
+      connection.end();
+      
+    });	
+}
---
+++ routes/connections.js
@@ -0,0 +1,7 @@
+exports.connectionString = {
+		host: 'us-cdbr-iron-east-03.cleardb.net',
+        user: 'b731924b38155d',
+        password : '7937a10b',
+        database : 'ad_8057bd3f5d9d5f8',
+        multipleStatements : true
+}
